9226bdaf88374b63bbff83155591d00ff5da7fb2
Cristian Mihai Iorga <cristian.mihai.iorga@kdi.kongsberg.com>
2018-05-04 16:17:58 +0200

    Test for SignalR support in Ocelot

---
+++ DummySignalR/DummySignalR/DummyHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DummySignalR
+{
+    public class DummyHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.SendAsync("broadcastMessage", name, message);
+        }
+    }
+}
---
+++ DummySignalR/DummySignalRClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace DummySignalRClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //var server = "http://localhost:50000/dummy";
+            var server = "http://localhost:60000/api/dummy";
+
+            var connection = new HubConnectionBuilder()
+                .WithUrl(server)
+                .WithConsoleLogger()
+                .Build();
+
+            await connection.StartAsync();
+
+            connection.On<string, string>("broadcastMessage", (name, message) =>
+            {
+                Console.WriteLine($"{name} said: {message}");
+            });
+
+            await connection.SendAsync("Send", new[] { "cristian", "dummy message from console app" });
+
+            Console.ReadLine();
+            await  connection.DisposeAsync();
+        }
+    }
+}
